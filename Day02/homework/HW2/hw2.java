ff85ec471b5d476c2bfb987e7061fd5fd3e465f8
2022ASP68 <chalakasithum421@gmail.com>
2025-03-22 21:34:51 +0530

    Add files via upload

---
+++ Day02/homework/HW2/hw2.java
@@ -0,0 +1,10 @@
+public class hw2{
+	public static void main(String args[]){
+		 int Num1 = 5004;
+		 double Num2 = 98.47;
+		 double Sum = Num1+Num2;
+		 
+		 System.out.println("The sum of "+Num1+" and "+Num2+" is: "+Sum);
+	}
+}	
+		
